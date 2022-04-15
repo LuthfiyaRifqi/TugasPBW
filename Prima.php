2af3e558255dfb7bc516409690ee0bd0a80b03b2
LuthfiyaRifqi <103103678+LuthfiyaRifqi@users.noreply.github.com>
2022-04-15 23:29:56 +0700

    Add files via upload

---
+++ Prima.php
@@ -0,0 +1,13 @@
+<form method ="POST" action = "proses.php" >
+    Mengetahui bilanagan prima atau bukan 
+    <br>
+
+    bilangan yang ingin diketahui : 
+    <input type = "text" name = "prima"/>
+    <br>
+
+    
+
+    <input type="submit" name ="submit" value ="cek"/>
+
+</form>
---
+++ Proses_Prima.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bilangan prima</title>
+</head>
+<body>
+    <form action="Prima.php" method="POST">
+        Masukkan bilangan: <br>
+        <input type="text" name="nilai" id=""> <br>
+        <input type="submit" name="" id="" value="Go">
+    </form>
+</body>
+</html>
